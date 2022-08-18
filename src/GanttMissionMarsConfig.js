2d9148fdaa47fc82cb2dd502e8f7257a953cdebc
Samir Medjdoub <samirmedjdoub@imac-de-samir-1.home>
2022-08-18 13:28:22 +0200

    Rendering important tasks with specific background + filterFeature instead of filter

--- src/GanttMissionMarsConfig.js
+++ src/GanttMissionMarsConfig.js
@@ -11,8 +11,14 @@ const ganttConfig = {
     type: 'gantttoolbar',
   },
   // to add filtering option on every column grid header
-  features: {
-    filter: true,
+  filterFeature: true,
+  taskRenderer({ taskRecord, renderData }) {
+    console.log('taskRenderer', { taskRecord, renderData });
+    console.log('taskRecord.name', taskRecord.name);
+    if (taskRecord.name.toLocaleLowerCase().startsWith('important')) {
+      // make important task red
+      renderData.style = 'background-color: red; color: pink';
+    }
   },
 };
 
